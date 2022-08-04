71f855054cdf43518a2d42a09bf08edfec35a64c
ToruMurakami <toru.murakami@zenken.co.jp>
2022-08-04 13:09:27 +0900

    SSR対応の記述に修正

--- pages/corporate/[slug].jsx
+++ pages/corporate/[slug].jsx
@@ -1,48 +1,65 @@
 import { useRouter } from 'next/router';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import Link from 'next/link';
-
+import Helmet from "react-helmet";
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import styles from '../../style/index.module.scss';
 
-export default function Post() {
-    const router = useRouter();
-    const { slug } = router.query;
-    //API呼び出し
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        //現在のURL(パス)を取得
-        const url = window.location.pathname;
-        //現在のURL(パス)から投稿IDを取得
-        const postId = url.split("/")[2];
-        console.log(postId);
-        fetch('https://www.next-doorrr.com/knowledge-box/wp-json/wp/v2/posts?slug=' + postId)
-            .then(res => res.json())
-            .then(json => {
-                setData(json);
-                console.log(json);
-            });
+//HTML
+const Post = (props) => {
+  const router = useRouter();
+  const { slug } = router.query;
+  return (
+    <div className="container">
+      <Helmet>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Helmet>
+      <Header />
+      <main className={styles.main}>
+        {props.data.map((data) => {
 
-    }, []);
-    console.log(slug);
+          return (
+            <div className="container">{console.log(data)}
+              {
+                data ? (<>
+                  <h2>{"カテ1-1：" + data.title.rendered}</h2>
+                  <p>WordPressAPIでスラッグ検索して該当のデータを絞った状態で表示</p>
+                  <div className="grid">
+                    {data.content.rendered}
+                  </div></>) : (<>データ読込中</>)
+              }
+              <Link href={"/corporate/" + slug + "/test"}>
+                <button>詳細へ</button>
+              </Link>
 
-    return (
-        <div className="container">
-            <Header />
-            <main className={styles.main}>
+            </div>
+          );
+        })}
+      </main >
 
-                {data?.length > 0 ? (<>
-                    <h2>{"カテ1-1：" + data[0].title.rendered}</h2>
-                    <p>WordPressAPIでスラッグ検索して該当のデータを絞った状態で表示</p>
-                    <div className="grid">
-                        {data[0].content.rendered}
-                    </div></>) : (<>データ読込中</>)}
-                <Link href={"/corporate/" + slug + "/test"}>
-                    <button>詳細へ</button>
-                </Link>
-            </main >
+      <Footer />
 
-            <Footer />
-        </div>);
+    </div >)
 }
+
+//API呼び出し
+Post.getInitialProps = async (context) => {
+
+  //URL(postID)を取得
+  const { asPath } = context;
+  const postId = asPath.split("/")[2];
+
+  const res = await fetch(
+    'https://www.next-doorrr.com/knowledge-box/wp-json/wp/v2/posts?slug=' + postId
+  );
+  const data = await res.json();
+
+
+
+  return {
+    data: data,
+  };
+};
+export default Post;
